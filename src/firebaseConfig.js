75492c8da5028996543d941e9cf320d4ea5f8049
algofactor <johnsolutions4@gmail.com>
2022-09-15 23:06:35 +0100

    New change

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQyqUGfepVaOusGl26tdnpLhfhtjQscUs",
+  authDomain: "instablog-b76cc.firebaseapp.com",
+  projectId: "instablog-b76cc",
+  storageBucket: "instablog-b76cc.appspot.com",
+  messagingSenderId: "55548545774",
+  appId: "1:55548545774:web:25a9dc9743a8fc28fc41ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
